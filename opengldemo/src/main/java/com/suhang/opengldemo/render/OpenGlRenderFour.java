b6c3a61ce8eb5419208904b8adc5c2d78f52cb0f
lightofrain <34757596@qq.com>
2017-03-07 22:42:44 +0800

    阶段4

--- opengldemo/src/main/java/com/suhang/opengldemo/render/OpenGlRenderFour.java
+++ opengldemo/src/main/java/com/suhang/opengldemo/render/OpenGlRenderFour.java
@@ -19,171 +19,151 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
-import static android.opengl.GLES20.GL_DEPTH_BUFFER_BIT;
 
 /**
  * Created by 苏杭 on 2017/2/5 13:54.
  */
 
 public class OpenGlRenderFour implements GLSurfaceView.Renderer {
-    public static final int VERTEX_COUNT = 3;
-    public static final int VERTEX_COLOR_COUNT = 3;
-    public static final int VERTEX_TEXTURE_COUNT = 2;
-    public static final int FLOATBYTE = 4;
-    public static final int STRIDE = (VERTEX_COUNT + VERTEX_COLOR_COUNT + VERTEX_TEXTURE_COUNT) * FLOATBYTE;
+	public static final int VERTEX_COUNT = 3;
+	public static final int VERTEX_COLOR_COUNT = 3;
+	public static final int VERTEX_TEXTURE_COUNT = 2;
+	public static final int FLOATBYTE = 4;
+	public static final int STRIDE = (VERTEX_COUNT + VERTEX_COLOR_COUNT + VERTEX_TEXTURE_COUNT) * FLOATBYTE;
 
-    Context mContext;
-    private float[] mVertex;
-    private int[] mIndices;
-    private FloatBuffer mVertexData;
-    private IntBuffer mIndicesData;
-    private int mProgram;
-    private int mTexture;
-    private int mTexture1;
-    private float[] mMatrix = new float[16];
-    private float[] mProjectionMatrix = new float[16];
-    private float[] mViewMatrix = new float[16];
-    private float[] mModelMatrix = new float[16];
-    private int mTransform;
-    private int mOutTexture;
-    private int mOutTexture1;
+	Context mContext;
+	private float[] mVertex;
+	private int[] mIndices;
+	private FloatBuffer mVertexData;
+	private IntBuffer mIndicesData;
+	private int mProgram;
+	private int mTexture;
+	private int mTexture1;
+	private float[] mMatrix = new float[16];
+	private float[] mProjectionMatrix = new float[16];
+	private float[] mViewMatrix = new float[16];
+	private float[] mModelMatrix = new float[16];
+	private int mModel;
+	private int mView;
+	private int mProjection;
+	private int mOutTexture;
+	private int mOutTexture1;
 
-    public OpenGlRenderFour(Context context) {
-        mContext = context;
-    }
-
-
-    @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        // Set the background color to black ( rgba ).
-        GLES31.glClearColor(0.0f, 0.0f, 0.0f, 1f);  // OpenGL docs.
-        GLES31.glEnable(GLES31.GL_DEPTH_TEST);
-    }
-
-    private void getLocations() {
-        mOutTexture = GLES31.glGetUniformLocation(mProgram, "outTexture");
-        mOutTexture1 = GLES31.glGetUniformLocation(mProgram, "outTexture1");
-        mTransform = GLES31.glGetUniformLocation(mProgram, "transform");
-    }
-
-    private void createData() {
-        mVertex = new float[]{
-                0.5f, 0.5f * factor, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // 右上
-                0.5f, -0.5f * factor, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,   // 右下
-                -0.5f, -0.5f * factor, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,   // 左下
-                -0.5f, 0.5f * factor, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f    // 左上
-        };
-
-        mIndices = new int[]{
-                0, 1, 3, // 第一个三角形
-                1, 2, 3  // 第二个三角形
-        };
-        mVertexData = ByteBuffer.allocateDirect(mVertex.length * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
-        mVertexData.put(mVertex);
-        mIndicesData = ByteBuffer.allocateDirect(mIndices.length * 4).order(ByteOrder.nativeOrder()).asIntBuffer();
-        mIndicesData.put(mIndices);
-        mIndicesData.position(0);
-        mTexture = TextureUtil.loadTexture(mContext, R.mipmap.ic_launcher_round);
-        mTexture1 = TextureUtil.loadTexture(mContext, R.mipmap.huanpeng);
-    }
-
-    private void init() {
-        mProgram = ShaderUtil.createProgram(ShaderUtil.createShader(mContext, R.raw.vertex_shader_four, GLES31.GL_VERTEX_SHADER), ShaderUtil.createShader(mContext, R.raw.fragment_shader_four, GLES31.GL_FRAGMENT_SHADER));
-        GLES31.glUseProgram(mProgram);
-    }
+	public OpenGlRenderFour(Context context) {
+		mContext = context;
+	}
 
 
-    private void createProjectionMatrix(int width, int height) {
-        float ratio = 1;
-        float left = -1;
-        float right = 1;
-        float bottom = -1;
-        float top = 1;
-        float near = 2;
-        float far = 12;
-        if (width > height) {
-            ratio = (float) width / height;
-            left *= ratio;
-            right *= ratio;
-        } else {
-            ratio = (float) height / width;
-            bottom *= ratio;
-            top *= ratio;
-        }
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// Set the background color to black ( rgba ).
+		GLES31.glClearColor(0.0f, 0.0f, 0.0f, 1f);  // OpenGL docs.
+//        GLES31.glEnable(GLES31.GL_DEPTH_TEST);
+	}
 
-        Matrix.frustumM(mProjectionMatrix, 0, left, right, bottom, top, near, far);
-    }
+	private void getLocations() {
+		mOutTexture = GLES31.glGetUniformLocation(mProgram, "outTexture");
+		mOutTexture1 = GLES31.glGetUniformLocation(mProgram, "outTexture1");
+		mModel = GLES31.glGetUniformLocation(mProgram, "model");
+		mView = GLES31.glGetUniformLocation(mProgram, "view");
+		mProjection = GLES31.glGetUniformLocation(mProgram, "projection");
+	}
 
-    private void createViewMatrix() {
-        // точка положения камеры
-        float eyeX = 0;
-        float eyeY = 2;
-        float eyeZ = 4;
+	private void createData() {
+		mVertex = new float[]{
+				0.5f, 0.5f , 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // 右上
+				0.5f, -0.5f , 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,   // 右下
+				-0.5f, -0.5f , 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,   // 左下
+				-0.5f, 0.5f , 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f    // 左上
+		};
 
-        // точка направления камеры
-        float centerX = 0;
-        float centerY = 0;
-        float centerZ = 0;
+		mIndices = new int[]{
+				0, 1, 3, // 第一个三角形
+				1, 2, 3  // 第二个三角形
+		};
+		mVertexData = ByteBuffer.allocateDirect(mVertex.length * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
+		mVertexData.put(mVertex);
+		mIndicesData = ByteBuffer.allocateDirect(mIndices.length * 4).order(ByteOrder.nativeOrder()).asIntBuffer();
+		mIndicesData.put(mIndices);
+		mIndicesData.position(0);
+		mTexture = TextureUtil.loadTexture(mContext, R.mipmap.ic_launcher_round);
+		mTexture1 = TextureUtil.loadTexture(mContext, R.mipmap.huanpeng);
+	}
 
-        // up-вектор
-        float upX = 0;
-        float upY = 1;
-        float upZ = 0;
+	private void init() {
+		mProgram = ShaderUtil.createProgram(ShaderUtil.createShader(mContext, R.raw.vertex_shader_four, GLES31.GL_VERTEX_SHADER), ShaderUtil.createShader(mContext, R.raw.fragment_shader_four, GLES31.GL_FRAGMENT_SHADER));
+		GLES31.glUseProgram(mProgram);
+	}
 
-        Matrix.setLookAtM(mViewMatrix, 0, eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
-    }
 
-    private void bindData() {
-        mVertexData.position(0);
-        GLES31.glVertexAttribPointer(0, VERTEX_COUNT, GLES31.GL_FLOAT, false, STRIDE, mVertexData);
-        GLES31.glEnableVertexAttribArray(0);
-        mVertexData.position(3);
-        GLES31.glVertexAttribPointer(1, VERTEX_COLOR_COUNT, GLES31.GL_FLOAT, false, STRIDE, mVertexData);
-        GLES31.glEnableVertexAttribArray(1);
-        mVertexData.position(6);
-        GLES31.glVertexAttribPointer(2, VERTEX_TEXTURE_COUNT, GLES31.GL_FLOAT, false, STRIDE, mVertexData);
-        GLES31.glEnableVertexAttribArray(2);
+	private void createProjectionMatrix(int width, int height) {
+		Matrix.setIdentityM(mProjectionMatrix,0);
+		Matrix.perspectiveM(mProjectionMatrix, 0, 45.0f, 1.0f * width / height, 0.1f, 100.0f);
+	}
 
-        GLES31.glActiveTexture(GLES31.GL_TEXTURE0);
-        GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, mTexture);
-        GLES31.glUniform1i(mOutTexture, 0);
-        GLES31.glActiveTexture(GLES31.GL_TEXTURE1);
-        GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, mTexture1);
-        GLES31.glUniform1i(mOutTexture1, 1);
-    }
+	private void createViewMatrix() {
+		Matrix.setIdentityM(mViewMatrix,0);
+		Matrix.translateM(mViewMatrix, 0, 0.0f, 0.0f, -3.0f);
+//		Matrix.setLookAtM(mViewMatrix, 0, 0, 0, 0f, 0, 0, 0, 0, 1, 1);
+	}
 
-    private float factor;
+	private void createModelMatrix() {
+		Matrix.setIdentityM(mModelMatrix,0);
+		Matrix.rotateM(mModelMatrix,0,-80.0f,1f,0.0f,0f);
+	}
 
-    @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
-        // Sets the current view port to the new size.
-        GLES31.glViewport(0, 0, width, height);// OpenGL docs.
-        factor = 1.0f * width / height;
-        init();
-        createData();
-        getLocations();
-        bindData();
-        createViewMatrix();
-        Matrix.setIdentityM(mModelMatrix, 0);
-        createProjectionMatrix(width, height);
-    }
-    long TIME = 10000L;
+	private void bindData() {
+		mVertexData.position(0);
+		GLES31.glVertexAttribPointer(0, VERTEX_COUNT, GLES31.GL_FLOAT, false, STRIDE, mVertexData);
+		GLES31.glEnableVertexAttribArray(0);
+		mVertexData.position(3);
+		GLES31.glVertexAttribPointer(1, VERTEX_COLOR_COUNT, GLES31.GL_FLOAT, false, STRIDE, mVertexData);
+		GLES31.glEnableVertexAttribArray(1);
+		mVertexData.position(6);
+		GLES31.glVertexAttribPointer(2, VERTEX_TEXTURE_COUNT, GLES31.GL_FLOAT, false, STRIDE, mVertexData);
+		GLES31.glEnableVertexAttribArray(2);
 
-    private void bindMatrix() {
+		GLES31.glActiveTexture(GLES31.GL_TEXTURE0);
+		GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, mTexture);
+		GLES31.glUniform1i(mOutTexture, 0);
+		GLES31.glActiveTexture(GLES31.GL_TEXTURE1);
+		GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, mTexture1);
+		GLES31.glUniform1i(mOutTexture1, 1);
+	}
+
+//	private float factor;
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// Sets the current view port to the new size.
+		GLES31.glViewport(0, 0, width, height);// OpenGL docs.
+//		factor = 1.0f * width / height;
+		init();
+		createData();
+		getLocations();
+		bindData();
+		createViewMatrix();
+		createModelMatrix();
+		createProjectionMatrix(width, height);
+//		Matrix.setIdentityM(mModelMatrix, 0);
+		bindMatrix();
+	}
+
+	long TIME = 10000L;
+
+	private void bindMatrix() {
         float angle = (float)(SystemClock.uptimeMillis() % TIME) / TIME * 360;
-        Matrix.rotateM(mModelMatrix,0,angle,0f,0.0f,1f);
-//        Matrix.translateM(mModelMatrix, 0, 0.5f, 0.0f, 0.0f);
-        GLES31.glUniformMatrix4fv(mTransform, 1, false, mModelMatrix, 0);
-    }
+        Matrix.rotateM(mModelMatrix, 0,angle, 0.0f, 1.0f,0.5f);
+		GLES31.glUniformMatrix4fv(mModel, 1, false, mModelMatrix, 0);
+		GLES31.glUniformMatrix4fv(mView, 1, false, mViewMatrix, 0);
+		GLES31.glUniformMatrix4fv(mProjection, 1, false, mProjectionMatrix, 0);
+	}
 
-    @Override
-    public void onDrawFrame(GL10 gl) {
-        gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  // OpenGL docs.0
-        Matrix.setIdentityM(mModelMatrix, 0);
-        bindMatrix();
-//		gl.glLoadIdentity();
-//		gl.glTranslatef(0, 0, -4);
-        GLES31.glDrawElements(GLES31.GL_TRIANGLES, 6, GLES31.GL_UNSIGNED_INT, mIndicesData);
-//		GLES31.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
-    }
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		gl.glClear(GL_COLOR_BUFFER_BIT);  // OpenGL docs.0
+		Matrix.setIdentityM(mModelMatrix, 0);
+		bindMatrix();
+		GLES31.glDrawElements(GLES31.GL_TRIANGLES, 6, GLES31.GL_UNSIGNED_INT, mIndicesData);
+	}
 }
--- opengldemo/src/main/res/raw/vertex_shader_four.glsl
+++ opengldemo/src/main/res/raw/vertex_shader_four.glsl
@@ -4,10 +4,13 @@ layout (location = 1) in vec3 color;
 layout (location = 2) in vec2 texPosition;
 out vec3 fragcolor;
 out vec2 outTex;
-uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-    gl_Position = transform * vec4(position, 1.0f);
+    gl_Position =projection* view * model * vec4(position, 1.0f);
     fragcolor = color;
     outTex = vec2(texPosition.x,1.0f-texPosition.y);
 }
